2a027afba67b11a356c4e82bc81cb9184578570d
Stefano <ste.albi_98@yahoo.it>
2020-08-06 16:16:07 +0200

    comments and documentation

--- server/configuration.h
+++ server/configuration.h
@@ -12,7 +12,6 @@ namespace net = boost::asio;
 
 namespace configuration
 {
-
     extern net::ip::address address;
     extern short unsigned port;
     extern int nthreads;
--- server/main.cpp
+++ server/main.cpp
@@ -2,33 +2,37 @@
 
 #include "Listener.h"
 #include "configuration.h"
-#include "dao.h"
 
 int main() {
 
-    // load the config file
+    // Load the configuration file
     if(!configuration::load_config_file("backupserver.conf")){
         return EXIT_FAILURE;
     }
 
-    // The io_context is required for all I/O
+
+    // The io_context is required for all I/O (including network)
     net::io_context ioc{configuration::nthreads};
 
-    // Create and launch a listening port
+    // Create and launch a Listener on the configuration port and address
     std::make_shared<Listener>(ioc,tcp::endpoint{configuration::address, configuration::port})->run();
 
 
-    // Capture SIGINT and SIGTERM to perform a clean shutdown
+    // Capture SIGINT and SIGTERM
+    // This code allow to perform a clean shutdown of the server (using a signal or ctrl+C)
     net::signal_set signals(ioc, SIGINT, SIGTERM);
     signals.async_wait(
-            [&](beast::error_code const&, int)
+            [&ioc](beast::error_code const& ec, int sign)
             {
                 // Stop the `io_context`. This will cause `run()`
                 // to return immediately, eventually destroying the
                 // `io_context` and all of the sockets in it.
-                std::cout << "Exit after signal" << std::endl;
+                std::stringstream ss;
+                ss << "Exit after signal with code " << sign << std::endl;
+                std::cout << ss.str();
                 ioc.stop();
             });
+
 
     // Run the I/O service on the requested number of threads
     std::vector<std::thread> v;
@@ -41,6 +45,7 @@ int main() {
     ioc.run();
 
 
+    //if run() returned -> a signal was sent (exit)
     // Block until all the threads exit
     for(auto& t : v)
         t.join();
