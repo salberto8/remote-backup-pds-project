833c0180271872a379376de4c0b896ba9ab42922
Stefano <ste.albi_98@yahoo.it>
2020-08-25 20:18:59 +0200

    clean handle request

--- server/server.h
+++ server/server.h
@@ -55,7 +55,7 @@ void handle_request(
             [&req](beast::string_view why)
             {
                 http::response<http::string_body> res{http::status::bad_request, req.version()};
-                res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+                //res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
                 res.set(http::field::content_type, "text/plain");
                 res.keep_alive(req.keep_alive());
                 res.body() = std::string(why);
@@ -68,11 +68,7 @@ void handle_request(
             [&req]()
             {
                 http::response<http::empty_body> res{http::status::not_found, req.version()};
-                //res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-                //res.set(http::field::content_type, "text/html");
                 res.keep_alive(req.keep_alive());
-                //res.body() = "The resource '" + std::string(target) + "' was not found.";
-                //res.prepare_payload();
                 return res;
             };
 
@@ -94,11 +90,7 @@ void handle_request(
             [&req]()
             {
                 http::response<http::empty_body> res{http::status::ok, req.version()};
-                //res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-                //res.set(http::field::content_type, "text/html");
                 res.keep_alive(req.keep_alive());
-                //res.body() = "The resource '" + std::string(target) + "' was not found.";
-                //res.prepare_payload();
                 return res;
             };
 
@@ -106,6 +98,7 @@ void handle_request(
             [&req](beast::string_view what)
             {
                 http::response<http::string_body> res{http::status::forbidden, req.version()};
+                //res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
                 res.set(http::field::content_type, "text/plain");
                 res.keep_alive(req.keep_alive());
                 res.body() = "Forbidden: '" + std::string(what) + "'";
@@ -120,128 +113,71 @@ void handle_request(
         req.method() != http::verb::delete_)
         return send(bad_request("Unknown HTTP-method"));
 
-    // GET
-    if(req.method() == http::verb::get) {
-        std::string path = req.target().to_string();
 
-        //check if authorized
-        auto auth = req[http::field::authorization];
-        if(auth.empty()){
-            return send(forbidden_response("Token needed"));
+    std::string req_path = req.target().to_string();
+
+    //avoid path traversal
+    if(req_path.find("..")!=std::string::npos)
+        return send(bad_request("Bad path"));
+
+    // POST (login)
+    if(req.method() == http::verb::post && req_path.rfind("/login", 0) == 0) {
+        //login request
+        json j = json::parse(req.body());
+
+        std::string username;
+        std::string password;
+
+        try {
+            username = j.at("username");
+            password = j.at("password");
         }
-        std::string token = auth.to_string();
-        std::optional<std::string> user = verifyToken(token);
-        if (!user.has_value()) {
-            //invalid token
-            return send(forbidden_response("Invalid token"));
-        }
-
-        //if starts with probe
-        if (path.rfind("/probefile/", 0) == 0) {
-            path = path.substr(11);
-
-            std::optional<std::string> digest_opt = get_file_digest(user.value(), path);
-
-            if(digest_opt){
-                //file exists
-                std::string digest = digest_opt.value();
-                http::response<http::string_body> res{
-                        http::status::ok,
-                        req.version(),
-                        digest};
-                //res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-                res.set(http::field::content_type, "text/plain");
-                res.content_length(digest.size());
-                res.keep_alive(req.keep_alive());
-                return send(std::move(res));
-            } else {
-                //file not found
-                return send(not_found());
-            }
+        catch (json::out_of_range &e) {
+            //missing login parameters
+            return send(bad_request("Missing login parameters"));
         }
 
+        if (verifyUserPassword(username, password)) {
+            // the user exists and the password is verified
 
-        /*
-        if (path.rfind("/probefolder/", 0) == 0) {
-            path = path.substr(13);
-            bool res = probe_directory(user.value(),path);
-            if(res){
-                //folder exists
-                return send(okay_response());
-            } else {
-                //folder not found
-                return send(not_found());
-            }
+            // create token
+            std::string token = createToken(32);
+
+            // save token related to user
+            if (!saveTokenToUser(username, token))
+                return send(server_error("Error in creating token to user"));
+
+            // send token
+            http::response<http::string_body> res{
+                    http::status::ok,
+                    req.version(),
+                    token};
+            res.set(http::field::content_type, "text/plain");
+            res.content_length(token.size());
+            res.keep_alive(req.keep_alive());
+            return send(std::move(res));
+        } else {
+            // if the verification of the password fails send error
+            return send(server_error("Authentication failed"));
         }
-         */
 
-        return send(not_found());
     }
 
-    // POST
-    if(req.method() == http::verb::post) {
-        std::string req_path = req.target().to_string();
+    //check if authorized
+    auto auth = req[http::field::authorization];
+    if(auth.empty()){
+        std::cout << "token empty" << std::endl;
+        return send(forbidden_response("Token needed"));
+    }
+    std::string token = auth.to_string();
+    std::optional<std::string> user = verifyToken(token);
+    if (!user.has_value()) {
+        std::cout << "invalid token" << std::endl;
+        return send(forbidden_response("Invalid token"));
+    }
 
-        //login request
-        if (req_path.rfind("/login", 0) == 0){
-            json j = json::parse(req.body());
-
-            std::string username;
-            std::string password;
-
-            try {
-                username = j.at("username");
-                password = j.at("password");
-            }
-            catch (json::out_of_range& e){
-                //missing login parameters
-                return send(bad_request("Missing login parameters"));
-            }
-
-            if (verifyUserPassword(username, password)){
-                // the user exists and the password is verified
-
-                // create token
-                std::string token = createToken(32);
-
-                // save token related to user
-                if(!saveTokenToUser(username, token))
-                    return send(server_error("Error in creating token to user"));
-
-                // send token
-                http::response<http::string_body> res{
-                        http::status::ok,
-                        req.version(),
-                        token};
-                res.set(http::field::content_type, "text/plain");
-                res.content_length(token.size());
-                res.keep_alive(req.keep_alive());
-                return send(std::move(res));
-            }
-            else {
-                // if the verification of the password fails send error
-                return send(server_error("Authentication failed"));
-            }
-        }
-
-
-        //check if authorized
-        auto auth = req[http::field::authorization];
-        if(auth.empty()){
-            std::cout << "token empty" << std::endl;
-            return send(forbidden_response("Token needed"));
-        }
-        std::string token = auth.to_string();
-        std::optional<std::string> user = verifyToken(token);
-        if (!user.has_value()) {
-            std::cout << "invalid token" << std::endl;
-            return send(forbidden_response("Invalid token"));
-        }
-
-        //avoid path traversal
-        if(req_path.find("..")!=std::string::npos)
-            return send(bad_request("Bad path"));
-
+    //POST (authenticated)
+    if(req.method() == http::verb::post){
         //if starts with backup
         if (req_path.rfind("/backup/", 0) == 0){
 
@@ -332,31 +268,40 @@ void handle_request(
         return send(bad_request("Illegal request"));
     }
 
+    // GET
+    if(req.method() == http::verb::get) {
+        //if starts with probe
+        if (req_path.rfind("/probefile/", 0) == 0) {
+            req_path = req_path.substr(11);
+
+            std::optional<std::string> digest_opt = get_file_digest(user.value(), req_path);
+
+            if(digest_opt){
+                //file exists
+                std::string digest = digest_opt.value();
+                http::response<http::string_body> res{
+                        http::status::ok,
+                        req.version(),
+                        digest};
+                //res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+                res.set(http::field::content_type, "text/plain");
+                res.content_length(digest.size());
+                res.keep_alive(req.keep_alive());
+                return send(std::move(res));
+            } else {
+                //file not found
+                return send(not_found());
+            }
+        }
+        return send(not_found());
+    }
+
+
     //DELETE
     if (req.method() == http::verb::delete_){
-        std::string req_path = req.target().to_string();
-
-        auto auth = req[http::field::authorization];
-        if(auth.empty()){
-            return send(forbidden_response("Token needed"));
-        }
-
-        std::string token = auth.to_string();
-        std::optional<std::string> user = verifyToken(token);
-
-        if (!user.has_value()) {
-            //invalid token
-            return send(forbidden_response("Invalid token"));
-        }
-
-
         //if starts with backup
         if (req_path.rfind("/backup/", 0) == 0){
             std::string path = req_path.substr(8);
-
-            //avoid path traversal
-            if(path.find("..")!=std::string::npos)
-                return send(bad_request("Bad path"));
 
             if(backup_delete(user.value(),path))
                 return send(okay_response());
@@ -366,51 +311,6 @@ void handle_request(
 
         return send(bad_request("Illegal request"));
     }
-
-
-    /*
-    // Request path must be absolute and not contain "..".
-    if( req.target().empty() ||
-        req.target()[0] != '/' ||
-        req.target().find("..") != beast::string_view::npos)
-        return send(bad_request("Illegal request-target"));
-    // Build the path to the requested file
-    std::string path = path_cat(doc_root, req.target());
-    if(req.target().back() == '/')
-        path.append("index.html");
-    // Attempt to open the file
-    beast::error_code ec;
-    http::file_body::value_type body;
-    body.open(path.c_str(), beast::file_mode::scan, ec);
-    // Handle the case where the file doesn't exist
-    if(ec == beast::errc::no_such_file_or_directory)
-        return send(not_found(req.target()));
-    // Handle an unknown error
-    if(ec)
-        return send(server_error(ec.message()));
-    // Cache the size since we need it after the move
-    auto const size = body.size();
-    // Respond to HEAD request
-    if(req.method() == http::verb::head)
-    {
-        http::response<http::empty_body> res{http::status::ok, req.version()};
-        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-        res.set(http::field::content_type, mime_type(path));
-        res.content_length(size);
-        res.keep_alive(req.keep_alive());
-        return send(std::move(res));
-    }
-    // Respond to GET request
-    http::response<http::file_body> res{
-            std::piecewise_construct,
-            std::make_tuple(std::move(body)),
-            std::make_tuple(http::status::ok, req.version())};
-    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-    res.set(http::field::content_type, mime_type(path));
-    res.content_length(size);
-    res.keep_alive(req.keep_alive());
-    return send(std::move(res));
-     */
 }
 
 
